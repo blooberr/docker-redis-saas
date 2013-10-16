1eda43d0cee1b20ca4b5e0d29255ad5454d0a324
blooberr <blooberr@gmail.com>
2013-10-16 03:12:45 +0000

    removed haml dependency

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ docker-redis-saas
 
 Software as a service example for docker inspired by the memcached example from the workshop.
 
+Working with Rails
+http://ruby.railstutorial.org/ (wonderful resource for getting started!)
+
+
--- rails/redis-saas/config/routes.rb
+++ rails/redis-saas/config/routes.rb
@@ -1,4 +1,5 @@
 RedisSaas::Application.routes.draw do
+  get "accounts/create"
   get "home/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
